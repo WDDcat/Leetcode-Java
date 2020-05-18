93768755e635f9499f51166853ac46db6ce3036b
WDDcat <syyx20046@126.com>
2020-05-18 13:07:19 +0800

    152 Maximum Product Subarray

--- src/main.java
+++ src/main.java
@@ -3,7 +3,8 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		_210CourseScheduleII solution = new _210CourseScheduleII();
+		_152MaximumProductSubarray solution = new _152MaximumProductSubarray();
+		
 		return;
 	}
 
