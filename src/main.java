81573e11f6be6a497611abac529b3b17fa10c230
WDDcat <syyx20046@126.com>
2020-05-19 10:37:01 +0800

    680. Valid Palindrome II

--- src/main.java
+++ src/main.java
@@ -3,7 +3,8 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		_152MaximumProductSubarray solution = new _152MaximumProductSubarray();
+		_680ValidPalindromeII solution = new _680ValidPalindromeII();
+		System.out.println(solution.validPalindrome("abca"));
 		
 		return;
 	}
