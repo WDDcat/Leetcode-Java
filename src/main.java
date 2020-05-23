bed37790a5efb713856fd0b1eca13622e00d3a7e
WDDcat <syyx20046@126.com>
2020-05-23 14:45:40 +0800

    76. Minimum Window Substring

--- src/main.java
+++ src/main.java
@@ -3,8 +3,8 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		_5LongestPalindromicSubstring solution = new _5LongestPalindromicSubstring();
-		System.out.println(solution.longestPalindrome("abaa"));
+		_76MinimumWindowSubstring solution = new _76MinimumWindowSubstring();
+		System.out.println(solution.minWindow("a", "b"));
 		
 		return;
 	}
