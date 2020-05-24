653e84f19ab6d4d94eb809edec7d8070804202a2
WDDcat <syyx20046@126.com>
2020-05-24 23:32:00 +0800

    4. Median of Two Sorted Arrays

--- src/main.java
+++ src/main.java
@@ -3,8 +3,8 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		_76MinimumWindowSubstring solution = new _76MinimumWindowSubstring();
-		System.out.println(solution.minWindow("a", "b"));
+		_4MedianofTwoSortedArrays solution = new _4MedianofTwoSortedArrays();
+		System.out.println(solution.findMiddle());
 		
 		return;
 	}
