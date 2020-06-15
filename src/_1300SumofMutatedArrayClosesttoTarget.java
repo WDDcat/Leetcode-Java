605992082c14133fd8f06e1b3294fe6ef4c76cdb
WDDcat <syyx20046@126.com>
2020-06-15 16:01:25 +0800

    1300. Sum of Mutated Array Closest to Target

--- src/_1300SumofMutatedArrayClosesttoTarget.java
+++ src/_1300SumofMutatedArrayClosesttoTarget.java
@@ -2,6 +2,19 @@ import java.util.Arrays;
 
 public class _1300SumofMutatedArrayClosesttoTarget {
 	public int findBestValue(int[] arr, int target) {
+		
+		/*
+		 * 	好像比题解还优乐一些？
+		 * 	
+		 * 	先排序
+		 * 	然后遍历
+		 * 	看（如果后面的数字都等于这个数的话）的总和超没超target
+		 * 	没超就下一个
+		 * 	超了就用target - （这个数前面的）总和 然后除以 后面上下的个数 得到均值
+		 * 	
+		 * 	第一次做的时候没考虑到加和的绝对值之差最小，就没考虑超出target时的情况
+		 * 	于是加了double判断求出的均值是否大于0.5的判断
+		 * */
 		Arrays.sort(arr);
 		int sum = 0;
 		for(int i = 0; i < arr.length; i++) {
