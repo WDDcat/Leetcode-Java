3ebfe35a1f89bf49b67c530fe790c29351576808
WDDcat <syyx20046@126.com>
2020-06-22 18:32:59 +0800

    Interview 16.18. Pattern Matching

--- src/main.java
+++ src/main.java
@@ -3,11 +3,9 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		_394DecodeString cache = new _394DecodeString();
+		Interview16_18PatternMatching test = new Interview16_18PatternMatching();
 		
-		System.out.println(cache.decodeString("3[a]2[bc]"));
-		System.out.println(cache.decodeString("3[a2[c]]"));
-		System.out.println(cache.decodeString("2[abc]3[cd]ef"));
+		System.out.println(test.patternMatching("abba", "catdogdogcat"));
 		return;
 		
 	}
