3ab035d6a9c496a3189d033626450ecbbc0c4548
WDDcat <syyx20046@126.com>
2020-06-23 20:29:51 +0800

    67. Add Binary

---
+++ src/_67AddBinary.java
@@ -0,0 +1,48 @@
+
+public class _67AddBinary {
+	public String addBinary(String a, String b) {
+		
+		/*
+		 * 	时空肯定没有人家位运算的高
+		 * 	但也算是提供了另一种不一样的方法
+		 * */
+		
+		if(a.length() < b.length()) {
+			String temp = b;
+			b = a;
+			a = temp;
+		}
+		while(b.length() < a.length()) {
+			b = "0" + b;
+		}
+		int length = a.length();
+		StringBuilder ans = new StringBuilder();
+		int carry = 0;
+		for(int i = 0; i < length; i++) {			
+			int cur = Integer.valueOf(a.charAt(a.length() - 1) - 48) + Integer.valueOf(b.charAt(b.length() - 1) - 48) + carry;
+			if(cur == 0 || cur == 1) {
+				ans.insert(0, cur);
+				carry = 0;
+			}
+			else if(cur == 2) {
+				ans.insert(0, "0");
+				carry = 1;
+			}
+			else {
+				ans.insert(0, "1");
+				carry = 1;
+			}
+			
+			a = a.substring(0, a.length() - 1);
+			b = b.substring(0, b.length() - 1);
+			
+			if(a.length() == 0 && b.length() == 0) {
+				if(carry == 1) {
+					ans.insert(0, "1");
+				}
+				break;
+			}
+		}
+		return ans.toString();
+    }
+}
--- src/main.java
+++ src/main.java
@@ -3,9 +3,9 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Interview16_18PatternMatching test = new Interview16_18PatternMatching();
+		_67AddBinary test = new _67AddBinary();
 		
-		System.out.println(test.patternMatching("abba", "catdogdogcat"));
+		System.out.println(test. addBinary("11", "1"));
 		return;
 		
 	}
