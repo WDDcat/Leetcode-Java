08e777e0326e343245a2cb7fb331f1eb2aff22e7
WDDcat <syyx20046@126.com>
2020-08-03 02:07:27 +0800

    415. Add Strings

---
+++ src/_415_Add_Strings.java
@@ -0,0 +1,26 @@
+
+public class _415_Add_Strings {
+	public String addStrings(String num1, String num2) {
+		
+		/*
+		 * 	非常简单的一道题
+		 * 	看了题解优化了一下代码
+		 * */
+		
+		StringBuilder sb = new StringBuilder();
+		int carry = 0;
+		int i = num1.length() - 1;
+		int j = num2.length() - 1;
+		while (i >= 0 || j >= 0 || carry != 0) {
+			if (i >= 0) {
+				carry += num1.charAt(i--) - '0';
+			}
+			if (j >= 0) {
+				carry += num2.charAt(j--) - '0';
+			}
+			sb.append(carry % 10);
+			carry /= 10;
+		}
+		return sb.reverse().toString();
+	}
+}
