7fb3a83f5eb8d51c543e1c2698eedc885595e11c
Wang Shiyu <syyx20046@126.com>
2022-04-11 09:25:36 +0800

    add notes

--- src/general_0300_0399/_357_Number_of_Different_Figures.java
+++ src/general_0300_0399/_357_Number_of_Different_Figures.java
@@ -1,6 +1,21 @@
 package general_0300_0399;
 
 public class _357_Number_of_Different_Figures {
+	
+	/*
+	 * n = 0, 只有 0 这一个数字, ans(0) = 1;
+	 * 
+	 * n = 1, 除了 0 以外, 还有 1-9 九个数字 ans(1) = 9 + ans(0) = 10;
+	 * 
+	 * n = 2: 第一位1-9, 第二位是除了第一位外的 0-9 所以是 ans(2) = 9*9 + ans(1) = 81 + 9 + 1 = 91;
+	 *   
+	 * n = 3:       1-9  (0-9)-1  (0-9)-2
+	 *                x     x      x          ans(3) = 9*9*8 + ans(2) = ...
+	 * 
+	 * n = 4:		1-9  (0-9)-1  (0-9)-2  (0-9)-3  ans(4) = 9*9*8*7 + ans(3)
+	 *                x       x       x       x
+	 * */
+	
 	public int countNumbersWithUniqueDigits(int n) {
 		if(n == 0) return 1;
 		int num = 9;
